13fe89b85ad25b72141028c3d4c88be596e54961
Nitin Paladiya <nj9666@gmail.com>
2020-05-25 00:01:16 +0530

    Vender atb DONE

--- src/app/views/pages/vender/vender-paymant/vender-paymant.component.ts
+++ src/app/views/pages/vender/vender-paymant/vender-paymant.component.ts
@@ -1,6 +1,7 @@
 // Angular
 import { Component, OnInit,  ViewChild, Inject} from '@angular/core';
 import { MatSort,MatPaginator,MatTableDataSource} from '@angular/material';
+import { SystemService } from '../../../../Shared/SystemService';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { MatSort,MatPaginator,MatTableDataSource} from '@angular/material';
   styleUrls: ['./vender-paymant.component.scss']
 })
 export class VenderPaymantComponent implements OnInit {
-  displayedColumns: string[] = ['DisplayBusinessName','VenderFullName','BankAccount', 'NeftId', 'PaymantAmount', 'PaymantDate'];
+  displayedColumns: string[] = ['displayBusinessName','venderFullName','bankAccount', 'transid', 'paymantDate', 'paymantAmount'];
   dataSource_vender_paymant = new MatTableDataSource<vender_paymantTbl>(ELEMENT_DATA_vender_paymant);
   @ViewChild('mat_pag_vender_paymant', {read: MatPaginator, static: true}) paginator_vender_paymant: MatPaginator;
   @ViewChild('mattbl_vender_paymant', {read: MatSort, static: true}) sort_vender_paymant: MatSort;
@@ -18,43 +19,36 @@ export class VenderPaymantComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource_vender_paymant.filter = filterValue.trim().toLowerCase();
   }
-  constructor() { }
+  constructor(public service: SystemService) { }
   ngOnInit() {
     this.dataSource_vender_paymant.paginator = this.paginator_vender_paymant;
     this.dataSource_vender_paymant.sort = this.sort_vender_paymant;
+    this.loadVender_p();
   }
-
+  loadVender_p(){
+    this.service.Data.ExecuteAPI_Get<any>("Vender/PayGetAll").then((data:any) =>
+		{
+      this.dataSource_vender_paymant = new MatTableDataSource<any>([]);
+      if (data.success)
+      {
+        ELEMENT_DATA_vender_paymant.length = 0;
+        data.data.forEach(element => { ELEMENT_DATA_vender_paymant.push(element); });
+        this.dataSource_vender_paymant = new MatTableDataSource<vender_paymantTbl>(ELEMENT_DATA_vender_paymant);
+        this.dataSource_vender_paymant.paginator = this.paginator_vender_paymant;
+        this.dataSource_vender_paymant.sort = this.sort_vender_paymant;
+      }
+		});
+  }
 }
 
 export class vender_paymantTbl {
-  Id: number;
-  DisplayBusinessName: string;
-  VenderFullName: string;
-  BankAccount: number;
-  NeftId: number;
-  PaymantAmount: number;
-  PaymantDate:string;
+  id: number;
+  displayBusinessName: string;
+  venderFullName: string;
+  bankAccount: number;
+  transid: number;
+  paymantAmount: number;
+  paymantDate:string;
 }
 
-const ELEMENT_DATA_vender_paymant: vender_paymantTbl[] = [
-  {Id:1, DisplayBusinessName:"Topicstorm", VenderFullName:"Forster Currington", BankAccount:138409974, NeftId:52201, PaymantAmount:389149, PaymantDate:"12/12/2019"},
-  {Id:2, DisplayBusinessName:"Realcube", VenderFullName:"Mitch Tommis", BankAccount:555410080, NeftId:16301, PaymantAmount:411149, PaymantDate:"12/02/2019"},
-  {Id:3, DisplayBusinessName:"Fadeo", VenderFullName:"Eugenie Teacy", BankAccount:584727651, NeftId:67768, PaymantAmount:187619, PaymantDate:"01/08/2020"},
-  {Id:4, DisplayBusinessName:"Wikizz", VenderFullName:"Brigham Oakenfull", BankAccount:608929309, NeftId:17632, PaymantAmount:54988, PaymantDate:"03/26/2020"},
-  {Id:5, DisplayBusinessName:"Twitterwire", VenderFullName:"Bill Snowding", BankAccount:429067148, NeftId:28581, PaymantAmount:666485, PaymantDate:"12/01/2019"},
-  {Id:6, DisplayBusinessName:"Innojam", VenderFullName:"Mae O'Fihillie", BankAccount:321650186, NeftId:66549, PaymantAmount:609435, PaymantDate:"06/15/2019"},
-  {Id:7, DisplayBusinessName:"Jayo", VenderFullName:"Missie Truluck", BankAccount:450327126, NeftId:10681, PaymantAmount:381286, PaymantDate:"11/20/2019"},
-  {Id:8, DisplayBusinessName:"Katz", VenderFullName:"Tyne Adamov", BankAccount:805488643, NeftId:66016, PaymantAmount:208111, PaymantDate:"03/08/2020"},
-  {Id:9, DisplayBusinessName:"Yadel", VenderFullName:"Clarabelle Cristofori", BankAccount:571793912, NeftId:23085, PaymantAmount:400522, PaymantDate:"09/20/2019"},
-  {Id:10, DisplayBusinessName:"Flipstorm", VenderFullName:"Courtenay Bluschke", BankAccount:440693203, NeftId:90244, PaymantAmount:341270, PaymantDate:"04/12/2020"},
-  {Id:11, DisplayBusinessName:"Wordware", VenderFullName:"Raimondo Whibley", BankAccount:162852455, NeftId:97642, PaymantAmount:945111, PaymantDate:"02/03/2020"},
-  {Id:12, DisplayBusinessName:"Skimia", VenderFullName:"Rolph Fosher", BankAccount:471425873, NeftId:15774, PaymantAmount:511386, PaymantDate:"03/26/2020"},
-  {Id:13, DisplayBusinessName:"Skipstorm", VenderFullName:"Florance Cleugher", BankAccount:610034703, NeftId:81864, PaymantAmount:116032, PaymantDate:"11/12/2019"},
-  {Id:14, DisplayBusinessName:"Jaxworks", VenderFullName:"Beret Paybody", BankAccount:603364666, NeftId:32342, PaymantAmount:140386, PaymantDate:"03/31/2020"},
-  {Id:15, DisplayBusinessName:"Oyonder", VenderFullName:"Clive Zuker", BankAccount:524210611, NeftId:71384, PaymantAmount:951979, PaymantDate:"12/16/2019"},
-  {Id:16, DisplayBusinessName:"Ailane", VenderFullName:"Giacinta Gomes", BankAccount:533345602, NeftId:89462, PaymantAmount:415528, PaymantDate:"06/05/2019"},
-  {Id:17, DisplayBusinessName:"Realbridge", VenderFullName:"Wyatt Skipton", BankAccount:574472274, NeftId:75902, PaymantAmount:998737, PaymantDate:"01/26/2020"},
-  {Id:18, DisplayBusinessName:"Skilith", VenderFullName:"Ade Whatham", BankAccount:287812740, NeftId:86112, PaymantAmount:772622, PaymantDate:"03/20/2020"},
-  {Id:19, DisplayBusinessName:"Jabberbean", VenderFullName:"Jackquelin Reckless", BankAccount:821268784, NeftId:43824, PaymantAmount:748496, PaymantDate:"11/28/2019"},
-  {Id:20, DisplayBusinessName:"Riffpath", VenderFullName:"Zerk Badder", BankAccount:533343273, NeftId:1640, PaymantAmount:709147, PaymantDate:"01/06/2020"},
-];
+const ELEMENT_DATA_vender_paymant: vender_paymantTbl[] = [];
--- src/app/views/pages/vender/venders/venders.component.ts
+++ src/app/views/pages/vender/venders/venders.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit,  ViewChild, Inject} from '@angular/core';
 import { MatSort,MatPaginator,MatTableDataSource} from '@angular/material';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
+import { SystemService } from '../../../../Shared/SystemService';
 
 @Component({
   selector: 'kt-venders',
@@ -10,7 +10,7 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
   styleUrls: ['./venders.component.scss']
 })
 export class VendersComponent implements OnInit {
-  displayedColumns: string[] = ['DisplayBusinessName','VenderFullName','MobileNumber', 'Email', 'PinCode', 'Paymant', 'PaymantAmount','actions'];
+  displayedColumns: string[] = ['displayBusinessName','venderFullName','mobileNumber', 'email',  'lastPayDate', 'paymant', 'paymantAmount','actions'];
   dataSource_vender = new MatTableDataSource<venderTbl>(ELEMENT_DATA_vender);
   @ViewChild('mat_pag_vender', {read: MatPaginator, static: true}) paginator_vender: MatPaginator;
   @ViewChild('mattbl_vender', {read: MatSort, static: true}) sort_vender: MatSort;
@@ -25,6 +25,9 @@ export class VendersComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
       console.log(result);
+      if(result != null){
+        this.PayVender(result.obj,result.transid);
+      }
     });
   }
 
@@ -32,13 +35,52 @@ export class VendersComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource_vender.filter = filterValue.trim().toLowerCase();
   }
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog,public service: SystemService) { }
 
   ngOnInit() {
     this.dataSource_vender.paginator = this.paginator_vender;
     this.dataSource_vender.sort = this.sort_vender;
+    this.loadvender();
   }
-
+  loadvender(){
+    this.service.Data.ExecuteAPI_Get<any>("Vender/GetAll").then((data:any) =>
+		{
+      this.dataSource_vender = new MatTableDataSource<any>([]);
+      if (data.success)
+      {
+        ELEMENT_DATA_vender.length = 0;
+        data.data.forEach(element => { ELEMENT_DATA_vender.push(element); });
+        this.dataSource_vender = new MatTableDataSource<venderTbl>(ELEMENT_DATA_vender);
+        this.dataSource_vender.paginator = this.paginator_vender;
+        this.dataSource_vender.sort = this.sort_vender;
+      }
+		});
+  }
+  PayVender(obj,transid){
+    
+    var sendobj = {
+      VenderId: obj.id,
+      BankAccount: obj.accountNumber,
+      TransactionId: transid,
+      Amount:obj.paymantAmount
+    }
+    console.log("Send--->>");
+    console.log(sendobj);
+    this.service.Data.ExecuteAPI<any>("Vender/PayInsert",sendobj).then((data:any) =>
+    {
+      
+    console.log("Recive--->>");
+    console.log(data);
+      if (data.success)
+      {
+        ELEMENT_DATA_vender.find(x => x.id == obj.id).paymant = 4;
+        ELEMENT_DATA_vender.find(x => x.id == obj.id).paymantAmount = 0;
+        this.dataSource_vender = new MatTableDataSource<venderTbl>(ELEMENT_DATA_vender);
+        this.dataSource_vender.paginator = this.paginator_vender;
+        this.dataSource_vender.sort = this.sort_vender;
+      }
+    });
+  }
 }
 
 
@@ -59,34 +101,21 @@ export class  venderPayDialog {
 }
 
 export class venderTbl {
-  Id: number;
-  DisplayBusinessName: string;
-  VenderFullName: string;
-  MobileNumber: number;
-  Email: string;
-  PinCode: number;
-  Paymant:number;
-  PaymantAmount:number;
+  id: number;
+  displayBusinessName: string;
+  venderFullName: string;
+  mobileNumber: number;
+  email: string;
+  paymant:number;
+  paymantAmount:number;
+  lastPayDate:Date;
+  accountHolderName:string;
+  accountNumber:number;
+  ifscCode:number;
+  bankName:string;
+  branch:string;
+  transid:string;
 }
 const ELEMENT_DATA_vender: venderTbl[] = [
-  { Id:1, DisplayBusinessName:"Voonyx", VenderFullName:"Tabatha Spratling", MobileNumber:2871131559, Email:"tspratling0@netlog.com", PinCode:771351, Paymant:2, PaymantAmount:12854},
-{ Id:2, DisplayBusinessName:"Npath", VenderFullName:"Ilka Arntzen", MobileNumber:5209096001, Email:"iarntzen1@ask.com", PinCode:884106, Paymant:3, PaymantAmount:33842},
-{ Id:3, DisplayBusinessName:"Youspan", VenderFullName:"Fabian Olexa", MobileNumber:1837736383, Email:"folexa2@foxnews.com", PinCode:440403, Paymant:1, PaymantAmount:34313},
-{ Id:4, DisplayBusinessName:"Eare", VenderFullName:"Artie Rembrant", MobileNumber:2762078869, Email:"arembrant3@jigsy.com", PinCode:187355, Paymant:4, PaymantAmount:2816},
-{ Id:5, DisplayBusinessName:"Skiba", VenderFullName:"Aeriel Janko", MobileNumber:2315858329, Email:"ajanko4@oaic.gov.au", PinCode:174610, Paymant:4, PaymantAmount:93289},
-{ Id:6, DisplayBusinessName:"Oyope", VenderFullName:"Berk Mayfield", MobileNumber:4115177720, Email:"bmayfield5@nydailynews.com", PinCode:434868, Paymant:1, PaymantAmount:80964},
-{ Id:7, DisplayBusinessName:"Wordpedia", VenderFullName:"Olvan Yearnsley", MobileNumber:7084023982, Email:"oyearnsley6@ocn.ne.jp", PinCode:366766, Paymant:1, PaymantAmount:1286},
-{ Id:8, DisplayBusinessName:"Trunyx", VenderFullName:"Tann Lark", MobileNumber:3891430764, Email:"tlark7@newyorker.com", PinCode:84721, Paymant:1, PaymantAmount:49266},
-{ Id:9, DisplayBusinessName:"Skynoodle", VenderFullName:"Maryl Sandbach", MobileNumber:8601295759, Email:"msandbach8@yellowpages.com", PinCode:725963, Paymant:2, PaymantAmount:43626},
-{ Id:10, DisplayBusinessName:"Midel", VenderFullName:"Thaine Pahler", MobileNumber:3317442962, Email:"tpahler9@technorati.com", PinCode:997666, Paymant:2, PaymantAmount:5390},
-{ Id:11, DisplayBusinessName:"Mudo", VenderFullName:"Jackie Boulder", MobileNumber:9744744140, Email:"jbouldera@dot.gov", PinCode:438416, Paymant:4, PaymantAmount:82970},
-{ Id:12, DisplayBusinessName:"Plajo", VenderFullName:"Homer McConigal", MobileNumber:5395210731, Email:"hmcconigalb@addthis.com", PinCode:523912, Paymant:2, PaymantAmount:52081},
-{ Id:13, DisplayBusinessName:"Twitterlist", VenderFullName:"Daryn Dacca", MobileNumber:5836892962, Email:"ddaccac@wikipedia.org", PinCode:644519, Paymant:1, PaymantAmount:75843},
-{ Id:14, DisplayBusinessName:"Chatterpoint", VenderFullName:"Gibby Humbee", MobileNumber:7815043899, Email:"ghumbeed@1688.com", PinCode:349927, Paymant:1, PaymantAmount:670},
-{ Id:15, DisplayBusinessName:"Zoombeat", VenderFullName:"Patrizia Duckinfield", MobileNumber:6869660805, Email:"pduckinfielde@apple.com", PinCode:644787, Paymant:2, PaymantAmount:5722},
-{ Id:16, DisplayBusinessName:"Voolia", VenderFullName:"Belva Abdon", MobileNumber:4419930858, Email:"babdonf@google.ru", PinCode:17437, Paymant:4, PaymantAmount:34628},
-{ Id:17, DisplayBusinessName:"Wordpedia", VenderFullName:"Hermia Marion", MobileNumber:3654050168, Email:"hmariong@discuz.net", PinCode:101667, Paymant:1, PaymantAmount:48949},
-{ Id:18, DisplayBusinessName:"LiveZ", VenderFullName:"Sherwood Rubinsky", MobileNumber:1741268762, Email:"srubinskyh@hp.com", PinCode:198036, Paymant:1, PaymantAmount:78298},
-{ Id:19, DisplayBusinessName:"Twiyo", VenderFullName:"Raquela Tewelson", MobileNumber:9244248000, Email:"rtewelsoni@apache.org", PinCode:829576, Paymant:4, PaymantAmount:98149},
-{ Id:20, DisplayBusinessName:"Youopia", VenderFullName:"Tami Cazin", MobileNumber:8952504297, Email:"tcazinj@chicagotribune.com", PinCode:35249, Paymant:2, PaymantAmount:31805},
+ 
 ];
